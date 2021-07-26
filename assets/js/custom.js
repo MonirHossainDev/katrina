6beb9bc112d0bc2669e1833bce338cb8b6f257c3
Monir Hossain <monirh.me@gmail.com>
2021-07-26 22:30:12 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,185 @@
+! function($) {
+    "use strict";
+
+    /* ---------------------------------------------- /*
+    * Preloader
+    /* ---------------------------------------------- */
+$('#year').text(new Date().getFullYear());
+    $(window).on('load', function() {
+        $('#preloader').addClass("loaded");
+    });
+
+    /* ---------------------------------------------- /*
+    * Section Scroll - Navbar
+    /* ---------------------------------------------- */
+    
+    $('.navbar-nav a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+        }, 1500, 'easeInOutExpo');
+
+        if($('.navbar').hasClass('active')){
+            $('.navbar').removeClass('active')
+            $('.ham').removeClass('active')
+        }
+
+        event.preventDefault();
+    });
+
+    $('.navbar-toggler').on('click', function(){
+        $('.aside').toggleClass('aside-open');
+        $(".ham").toggleClass('active');
+    });
+
+
+    $(window).on("scroll",function() {
+
+
+        if($(window).width() > 767 ) {
+            if ($(this).scrollTop() > 100 ) {
+                $('.overlay-wrap').addClass("in");
+            } else {
+                $('.overlay-wrap').removeClass("in");
+            }
+        }
+
+        if ($(this).scrollTop() > 500 ) {
+            $('.navbar').fadeIn(400);
+        } else {
+            $('.navbar').fadeOut(400);
+        }
+
+
+    });
+
+    /* ---------------------------------------------- /*
+    * Scroll Spy - init
+    /* ---------------------------------------------- */
+
+    $("#navbarCollapse").scrollspy({
+        offset:20
+    });
+
+    /* ---------------------------------------------- /*
+    * Magnific Popup - Init
+    /* ---------------------------------------------- */
+
+    $('.simple-ajax-popup').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        zoom: {
+            enabled: true,
+
+            duration: 300,
+            easing: 'ease-in-out',
+
+            opener: function(openerElement) {
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+    });
+
+    /* ---------------------------------------------- /*
+    * Swipper - Init
+    /* ---------------------------------------------- */
+
+    // Portolio init
+
+    var swiper = new Swiper('.swiper-portfolio', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        spaceBetween: -30,
+        initialSlide: 2,
+        coverflowEffect: {
+            rotate: 25,
+            stretch: 0,
+            depth: 150,
+            slideShadows : false,
+        },
+        breakpoints: {
+            767: {
+                slidesPerView: 2,
+            }
+        },
+        pagination: {
+            el: '.portfolio-pagination',
+            dynamicBullets: true,
+        },
+    });
+
+    // Testimony init
+
+    var swipertest = new Swiper('.swiper-testimony', {
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // Blog Init
+
+    var swipert = new Swiper('.swiper-blog', {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-progressbar',
+            type: 'progressbar',
+          },
+    });
+
+    // Home Banner Init
+
+    var swiper = new Swiper('.swiper-home', {
+        spaceBetween: 3,
+        effect: 'fade',
+        pagination: {
+            el: '.home-pagination',
+            clickable: true,
+        }
+    });
+
+    /* ---------------------------------------------- /*
+    * AnimateOnScroll - Init
+    /* ---------------------------------------------- */
+
+    var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+          // the callback is fired every time an animation is started
+          // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null,    // optional scroll container selector, otherwise use window,
+        resetAnimation: true,     // reset animation on end (default is true)
+      }
+    );
+    wow.init();
+
+    /* ---------------------------------------------- /*
+    * Home banner Scaler
+    /* ---------------------------------------------- */
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        $("#home").css({
+            transform: 'translate3d(0, +'+(scroll/100)+'%, 0) scale('+(100 + scroll/10)/100+')'
+        });
+    });
+
+
+}(window.jQuery);
+
